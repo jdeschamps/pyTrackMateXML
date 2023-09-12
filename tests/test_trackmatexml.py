b22cf228f41bc8537e3dae95babc1f03ae58a645
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-09-12 11:19:32 +0200

    WIP: Fix test

--- tests/test_trackmatexml.py
+++ tests/test_trackmatexml.py
@@ -1,6 +1,6 @@
-from trackmatexml import trackmatexml
+from trackmatexml import TrackmateXML
 
 def test_empty_import(empty_xml):
     """Test that TrackmateXML can load an empty xml"""
     assert empty_xml.exists()
-    trackmatexml().loadfile(empty_xml)
+    TrackmateXML().loadfile(empty_xml)
